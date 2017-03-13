fa2427b0229820fd8c5390f931f448e5235b1ea8
philipp1996 <pmoedritscher@yahoo.com>
2017-03-13 08:15:44 +0100

    First Commit

---
+++ app/src/main/java/at/aau/moedritscher/divisionsrechner/MainActivity.java
@@ -0,0 +1,13 @@
+package at.aau.moedritscher.divisionsrechner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

2292e7a7e78e73c5a409d1f876c7ad4d3d793823
philipp1996 <pmoedritscher@yahoo.com>
2017-03-13 08:49:05 +0100

    Implementation added

--- app/src/main/java/at/aau/moedritscher/divisionsrechner/MainActivity.java
+++ app/src/main/java/at/aau/moedritscher/divisionsrechner/MainActivity.java
@@ -1,13 +1,57 @@
 package at.aau.moedritscher.divisionsrechner;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText eTDivd;
+    EditText eTDivr;
+
+    TextView tVOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eTDivd = (EditText) findViewById(R.id.eTDivd);
+        eTDivr = (EditText) findViewById(R.id.eTDivr);
+        tVOut = (TextView) findViewById(R.id.tVOut);
+
+
     }
+
+    public void onClickDiv(View view){
+
+
+        double divd = Double.parseDouble(eTDivd.getText().toString());
+        double divr = Double.parseDouble(eTDivr.getText().toString());
+
+        if(divr!=0){
+
+            double out = divd/divr;
+            tVOut.setText(""+out);
+        }else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+            builder.setMessage("Divison durch 0")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                        }
+                    });
+
+            builder.create();
+        }
+
+
+
+    }
+
+
+
 }
